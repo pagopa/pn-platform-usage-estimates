d517b71c6c89fe6a530652801bbd3388df57a5cc
federica.bellini <ACT3UI0@posteitaliane.it>
2023-04-20 11:16:59 +0200

    fix create implementation + added logs

--- src/main/java/it/pagopa/pn/platform/utils/TimelineGenerator.java
+++ src/main/java/it/pagopa/pn/platform/utils/TimelineGenerator.java
@@ -5,12 +5,15 @@ import it.pagopa.pn.platform.middleware.db.entities.PnEstimate;
 import it.pagopa.pn.platform.model.Month;
 import it.pagopa.pn.platform.model.TimelineEstimate;
 import it.pagopa.pn.platform.rest.v1.dto.EstimateDetail;
+import lombok.extern.slf4j.Slf4j;
 
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+@Slf4j
 
 public class TimelineGenerator {
 
@@ -27,18 +30,25 @@ public class TimelineGenerator {
 
     public TimelineEstimate extractAllEstimates(Instant onboardingDate, String paId) {
         Collections.sort(dbList);
+        log.info("Prendo i dati da db: {}", dbList.size());
         Instant currentDate = DateUtils.getStartDeadLineDate();
+        log.info("startDeadlineDate: {} ",currentDate);
         int lastMonth = 0;
         while (currentDate.isAfter(onboardingDate)){
                 if (this.dbList != null && !this.dbList.isEmpty() && lastMonth < this.dbList.size()
                         && DateUtils.isEqualMonth(currentDate, this.dbList.get(lastMonth).getDeadlineDate())) {
+                    log.info("Sto per aggiungere stima presente a db nello storico: {}", currentDate);
                     this.timelineList.add(this.dbList.get(lastMonth));
                     lastMonth++;
                 } else {
+                    log.info("Aggiungo stima generata per {} deadlineDate", currentDate);
                     this.timelineList.add(getEstimate(this.paId, null, currentDate));
                 }
+
             currentDate = DateUtils.minusMonth(currentDate, 1);
+            log.info("Decrementata currentDate di un mese: {}", currentDate);
         }
+        log.info("Ritorno storico completo {}", timelineList.size());
         return new TimelineEstimate(this.timelineList.get(0), timelineList.subList(1, timelineList.size()));
     }
 
@@ -51,17 +61,19 @@ public class TimelineGenerator {
      */
     public static PnEstimate getEstimate(String paId, String referenceMonth, Instant deadline){
         if (referenceMonth == null && deadline == null) throw new AssertionError();
-
+        log.info("Creo la stima.");
         PnEstimate estimate = new PnEstimate();
         estimate.setPaId(paId);
         estimate.setLastModifiedTimestamp(Instant.now());
         if (deadline == null) {
+            log.info("caso in cui non mi è stato passata deadlineDate");
             String[] splitMonth = referenceMonth.split("-");
             int numberOfMonth = Month.getNumberMonth(splitMonth[0]);
             Instant refMonthInstant = DateUtils.fromDayMonthYear(15, numberOfMonth, Integer.parseInt(splitMonth[1]));
             estimate.setDeadlineDate(DateUtils.minusMonth(refMonthInstant, 1));
             estimate.setReferenceMonth(referenceMonth);
         } else if (referenceMonth == null) {
+            log.info("caso in cui non mi è stato passato referenceMonth");
             Instant refMonthInstant = DateUtils.addOneMonth(deadline);
 
             String refMonth = Month.getValueFromNumber(DateUtils.getMonth(refMonthInstant));
@@ -74,6 +86,7 @@ public class TimelineGenerator {
 
         estimate.setStatus(EstimateDetail.StatusEnum.DRAFT.getValue());
         if (estimate.getDeadlineDate().isBefore(Instant.now())){
+            log.info("stima assente");
             estimate.setStatus(EstimateDetail.StatusEnum.ABSENT.getValue());
         }
 
