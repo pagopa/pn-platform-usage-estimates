f6771b7371b6a5c6c0ed6748e53b1ba1b24af918
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-04-20 12:19:38 +0200

    minor change

--- src/main/java/it/pagopa/pn/platform/exception/ExceptionTypeEnum.java
+++ src/main/java/it/pagopa/pn/platform/exception/ExceptionTypeEnum.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ExceptionTypeEnum {
     ESTIMATE_NOT_EXISTED("ESTIMATE_NOT_EXISTED", "La stima non è presente nel sistema"),
+    REFERENCE_MONTH_NOT_CORRECT("REFERENCE_MONTH_NOT_CORRECT", "Il mese di riferimento non è nel formato corretto"),
     PA_ID_NOT_EXIST("PA_ID_NOT_EXIST", "La pa non è presente a sistema"),
     BILLING_NOT_EXIST("BILLING_NOT_EXIST", "Fatturazione non presente a sistema");
 
