440acb9f92b70e8db4db46af0d34b7e4fec7e771
cristopher.moccia <cristopher.moccia@accenture.com>
2023-06-22 17:16:18 +0200

    changed aws batch

--- src/main/java/it/pagopa/pn/platform/service/impl/AwsBatchServiceImpl.java
+++ src/main/java/it/pagopa/pn/platform/service/impl/AwsBatchServiceImpl.java
@@ -1,12 +1,13 @@
 package it.pagopa.pn.platform.service.impl;
 
+import com.amazonaws.services.batch.AWSBatch;
+import com.amazonaws.services.batch.model.SubmitJobRequest;
+import com.amazonaws.services.batch.model.SubmitJobResult;
 import it.pagopa.pn.platform.config.PnPlatformConfig;
 import it.pagopa.pn.platform.service.AwsBatchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.batch.BatchClient;
-import software.amazon.awssdk.services.batch.model.SubmitJobRequest;
 import software.amazon.awssdk.services.batch.model.SubmitJobResponse;
 
 import java.util.HashMap;
@@ -17,29 +18,29 @@ import java.util.UUID;
 @Service
 public class AwsBatchServiceImpl implements AwsBatchService {
     @Autowired
-    private BatchClient awsBatch;
+    private AWSBatch awsBatch;
     @Autowired
     private PnPlatformConfig pnPlatformConfig;
 
     @Override
     public String scheduleJob(String paId, String bucket, String reportKey) {
-        log.info("Schedule a new JOB for {} paId and {} reportKey", paId, reportKey);
+        log.info("Schedule a new JOB for {} paId and {} fileKey", paId, reportKey);
         Map<String, String> map = new HashMap<>();
         map.put("PA_ID", paId);
-        map.put("PN_PLATFORMUSAGEESTIMATES_DATALAKEBUCKETNAME", bucket);
-        map.put("PN_PLATFORMUSAGEESTIMATES_DATALAKEREPORTKEY", reportKey);
+        map.put("BUCKET_NAME", bucket);
+        map.put("FILE_KEY", reportKey);
 
         log.debug("Job Queue : {}", pnPlatformConfig.getJobQueueName());
         log.debug("Job Definition : {}", pnPlatformConfig.getJobDefinitionName());
+        log.debug("Properties : {}", pnPlatformConfig);
 
-        SubmitJobRequest jobRequest = SubmitJobRequest.builder()
-                .jobName(UUID.randomUUID().toString())
-                .jobQueue(pnPlatformConfig.getJobQueueName())
-                .jobDefinition(pnPlatformConfig.getJobDefinitionName())
-                .parameters(map)
-                .build();
-        
-        SubmitJobResponse result = awsBatch.submitJob(jobRequest);
-        return result.jobId();
+        com.amazonaws.services.batch.model.SubmitJobRequest jobRequest = new SubmitJobRequest()
+                .withJobName(UUID.randomUUID().toString())
+                .withJobQueue(pnPlatformConfig.getJobQueueName())
+                .withJobDefinition(pnPlatformConfig.getJobDefinitionName())
+                .withParameters(map);
+
+        SubmitJobResult result = awsBatch.submitJob(jobRequest);
+        return result.getJobId();
     }
 }
