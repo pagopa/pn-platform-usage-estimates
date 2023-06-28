3dd74ebaed254938470c1099ec15aeb6dc9bd2e8
federica.bellini <ACT3UI0@posteitaliane.it>
2023-06-28 09:50:08 +0200

    initialize fileMapper junitTest

---
+++ src/test/java/it/pagopa/pn/platform/mapper/FileMapperTest.java
@@ -0,0 +1,27 @@
+package it.pagopa.pn.platform.mapper;
+
+import org.junit.jupiter.api.Test;
+
+
+class FileMapperTest {
+
+    @Test
+    void toDownloadFile() {
+    }
+
+    @Test
+    void fromPnActivityReportToInfoDownloadDTO() {
+    }
+
+    @Test
+    void toPageableResponse() {
+    }
+
+    @Test
+    void toPagination() {
+    }
+
+    @Test
+    void deanonymizedFilesToDto() {
+    }
+}
