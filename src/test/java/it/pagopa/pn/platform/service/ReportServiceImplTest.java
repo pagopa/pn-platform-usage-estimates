4892fe9ed8705bd98adebdf2006fe02f1b0dce69
oliviero.caico <oliviero.caico@accenture.com>
2023-07-14 12:11:47 +0200

    fix import

--- src/test/java/it/pagopa/pn/platform/service/ReportServiceImplTest.java
+++ src/test/java/it/pagopa/pn/platform/service/ReportServiceImplTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-git statimport reactor.core.CoreSubscriber;
+import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
