6a0588a41469446428b9d57b93fe4b28743a5624
oliviero.caico <oliviero.caico@accenture.com>
2023-07-31 17:46:19 +0200

    fix test

--- src/test/java/it/pagopa/pn/platform/PnEstimatesTestConfig.java
+++ src/test/java/it/pagopa/pn/platform/PnEstimatesTestConfig.java
@@ -28,15 +28,41 @@ class PnEstimatesTestConfig {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(pnPlatformConfig.getClass().getSimpleName());
         stringBuilder.append("(");
-        stringBuilder.append("clientNationalRegistriesBasepath=");
-        stringBuilder.append(clientExternalRegistriesBasepath);
+        stringBuilder.append("clientSafeStorageBasepath=");
+        stringBuilder.append(pnPlatformConfig.getClientSafeStorageBasepath());
+        stringBuilder.append(", ");
+        stringBuilder.append("clientExternalRegistriesBasepath=");
+        stringBuilder.append(pnPlatformConfig.getClientExternalRegistriesBasepath());
         stringBuilder.append(", ");
         stringBuilder.append("xPagopaExtchCxId=");
-        stringBuilder.append(xPagopaExtchCxId);
+        stringBuilder.append(pnPlatformConfig.getXPagopaExtchCxId());
+        stringBuilder.append(", ");
+        stringBuilder.append("safeStorageCxId=");
+        stringBuilder.append(pnPlatformConfig.getSafeStorageCxId());
+        stringBuilder.append(", ");
+        stringBuilder.append("clientDataVaultBasepath=");
+        stringBuilder.append(pnPlatformConfig.getClientDataVaultBasepath());
+        stringBuilder.append(", ");
+        stringBuilder.append("datalakeBucketName=");
+        stringBuilder.append(pnPlatformConfig.getDatalakeBucketName());
+        stringBuilder.append(", ");
+        stringBuilder.append("datalakeReportKey=");
+        stringBuilder.append(pnPlatformConfig.getDatalakeReportKey());
+        stringBuilder.append(", ");
+        stringBuilder.append("jobQueueName=");
+        stringBuilder.append(pnPlatformConfig.getJobQueueName());
+        stringBuilder.append(", ");
+        stringBuilder.append("jobDefinitionName=");
+        stringBuilder.append(pnPlatformConfig.getJobDefinitionName());
+        stringBuilder.append(", ");
+        stringBuilder.append("attemptDataVault=");
+        stringBuilder.append(pnPlatformConfig.getAttemptDataVault());
         stringBuilder.append(")");
         String toTest = stringBuilder.toString();
         Assertions.assertEquals(toTest, pnPlatformConfig.toString());
     }
+
+    //PnPlatformConfig(clientNationalRegistriesBasepath=clientNationalRegistriesBasepath, xPagopaExtchCxId=xPagopaExtchCxId)
 
     private PnPlatformConfig initPnPlatformConfig() {
         PnPlatformConfig pnPlatformConfig = new PnPlatformConfig();
